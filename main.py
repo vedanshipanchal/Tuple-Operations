600370a74e29823918f88885b6d59f18f0072b68
vedanshipanchal <vedanshipanchal@gmail.com>
2024-11-15 16:24:28 +0000

    turple Operations

---
+++ main.py
@@ -0,0 +1,16 @@
+#1
+x = ("Vedanshi",7,True,8.34)
+print(x)
+#2
+x = (2,3,4,5.6,7)
+print(x)
+#3
+x = x+(9,)
+print(x)
+#4
+x = (50,30,30,40)
+print(x.count(30))
+#5
+x= (2,3,3,4,5,6,7)
+slice = x[2:5]
+print(slice)
